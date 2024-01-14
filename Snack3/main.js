daf1db3371017481719747757f520fe650f47e1b
AlessioPirrotta <alessiopirrotta92@gmail.com>
2024-01-14 18:46:17 +0100

    snack3 completed

---
+++ Snack3/main.js
@@ -0,0 +1,27 @@
+// Ciclo For
+// snack 3
+// Crea due array che hanno un numero di elementi diversi. 
+// Aggiungi elementi all’array che ha meno elementi fino a quando
+//  ne avrà tanti quanti l’altro
+
+let frutti = ["Mela", "Banana", "Arancia", "Kiwi", "Uva", "Fragola"]
+let colori = ["Rosso", "Blu", "Verde", "Giallo"]
+let buttonA= document.getElementById("btn")
+let inserisciColore;
+let i=0
+
+for(colori[i] ; frutti.length !== colori.length; i++){
+ inserisciColore =  prompt("inserisci un colore")
+ colori.push (inserisciColore)
+ console.log(colori)
+}
+
+buttonA.addEventListener("click", function(){
+    location.reload()
+
+})
+
+
+
+
+
